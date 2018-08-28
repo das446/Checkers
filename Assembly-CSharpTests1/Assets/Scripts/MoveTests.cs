05d6762b980f7dc2645bab230ca92ea07906ea72
nintendavid26 <nintendavid26@aol.com>
2018-08-27 22:30:54 -0400

    More unit testing

---
+++ Assembly-CSharpTests1/Assets/Scripts/MoveTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CheckersLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersLogic.Tests
+{
+    [TestClass()]
+    public class MoveTests
+    {
+        [TestMethod()]
+        public void MoveTest()
+        {
+            Position p1 = new Position(0, 0);
+            Position p2 = new Position(1, 1);
+            Move m = new Move(p1, p2);
+            Assert.IsNotNull(m);
+        }
+    }
+}
